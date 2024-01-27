26c6c1078458dd32d0f5d3109f372d871545b9c6
Oliver Coates <oliver.gcoates@gmail.com>
2024-01-27 19:00:35 +1300

    Rotation working!!!!!

--- Assets/Gun/Scripts/GunSightController.cs
+++ Assets/Gun/Scripts/GunSightController.cs
@@ -10,9 +10,7 @@ public class GunSightController : MonoBehaviour
     [SerializeField] private Transform _gunBaseTransform;
     [SerializeField] private Transform _gunSightReflector;
     private Transform selectedSightTransform;
-    private Transform selectedSightOldParent;
 
- 
 
     [Header("Base Transform:")]
     [SerializeField] private Vector3 _baseGunPosition;
@@ -31,6 +29,7 @@ public class GunSightController : MonoBehaviour
     private void Awake()
     {
         _baseGunPosition = _gunBaseTransform.localPosition;
+        _baseGunRotation = Quaternion.identity;
 
         if (_gunSights == null)
         {
@@ -48,16 +47,21 @@ public class GunSightController : MonoBehaviour
             _sightRotation = _baseGunRotation;
             return;
         }
+        
         selectedSightTransform = _gunSights[Random.Range(0, _gunSights.Count)]; 
 
-        _gunSightReflector.transform.localPosition = Vector3.zero;
-        _gunSightReflector.transform.localPosition = -_gunBaseTransform.InverseTransformPoint(selectedSightTransform.position);
-
-        _gunSightReflector.transform.localRotation = Quaternion.identity;
+        _gunBaseTransform.transform.localPosition = Vector3.zero;
+        
+        //_gunSightReflector.transform.localPosition = -_gunBaseTransform.InverseTransformPoint(selectedSightTransform.position);
+        _sightPosition = -_gunBaseTransform.InverseTransformPoint(selectedSightTransform.position);
 
         Vector3 forward = _gunBaseTransform.InverseTransformDirection(selectedSightTransform.forward);
         Vector3 up = _gunBaseTransform.InverseTransformDirection(selectedSightTransform.up);
-        _gunSightReflector.transform.localRotation = Quaternion.LookRotation(forward, up);
+
+        Vector3 invertedRotation = Quaternion.LookRotation(forward, up).eulerAngles;
+
+        //transform.localRotation = Quaternion.Euler(invertedRotation.x, invertedRotation.y, -invertedRotation.z);
+        _sightRotation = Quaternion.Euler(invertedRotation.x, invertedRotation.y, -invertedRotation.z);
     }
 
     public void AddSight(Transform newSight)
@@ -85,7 +89,10 @@ public class GunSightController : MonoBehaviour
         _aimingTimer = Mathf.Clamp(_aimingTimer, 0, _aimTime);
         float lerpAmount = _aimingTimer / _aimTime;
 
-        _gunBaseTransform.localPosition = _sightPosition;
+        //_gunBaseTransform.localPosition = _sightPosition;
+        _gunBaseTransform.localPosition = Vector3.Lerp(_baseGunPosition, _sightPosition, lerpAmount);
+
+        transform.localRotation = Quaternion.Slerp(_baseGunRotation, _sightRotation, lerpAmount);
 
         //_gunBaseTransform.localPosition = Vector3.Lerp(_baseGunPosition, Vector3.zero, lerpAmount);
         //old: _gunTransform.rotation = Quaternion.Euler(Vector3.Lerp(_baseGunRotation.eulerAngles, _sightRotation.eulerAngles, lerpAmount));
