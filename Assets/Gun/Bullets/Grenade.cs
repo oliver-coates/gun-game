c749385b05a989aacca28e4423d074606c49bc3f
Oliver Coates <oliver.gcoates@gmail.com>
2024-01-28 13:21:15 +1300

    Grenades cause explosions

--- Assets/Gun/Bullets/Grenade.cs
+++ Assets/Gun/Bullets/Grenade.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 
 public class Grenade : MonoBehaviour
 {
+    public GameObject _explosionPrefab;
+
+
     public float explosionRadius = 1.75f;
     public float minDamage = 80f;
     public float maxDamage = 200f;
+    private bool hasExploded = false;
 
     private void OnCollisionEnter(Collision other)
     {
-        
+        if (!hasExploded)
+        {
+            hasExploded = true;
+            StartCoroutine(CExplode());
+
+        }
+    }
+
+    private IEnumerator CExplode()
+    {
         Collider[] hits = Physics.OverlapSphere(transform.position, explosionRadius);
 
         foreach (Collider hit in hits)
@@ -23,7 +36,11 @@ public class Grenade : MonoBehaviour
             }
         }
 
-        Destroy(gameObject);
+        GameObject explosion = Instantiate(_explosionPrefab, transform.position, Random.rotation);
 
+        yield return new WaitForSeconds(2f);
+
+        Destroy(explosion);
+        Destroy(gameObject);
     }
 }
